e1e36b638b39fcf1ee31b51c33970cab0bbaefae
harshgj <87390466+harshgj@users.noreply.github.com>
2021-07-16 14:15:13 +0530

    Update BaseSteps.java

--- src/main/java/cucumberIntegrationTests/stepDefinitions/common/BaseSteps.java
+++ src/main/java/cucumberIntegrationTests/stepDefinitions/common/BaseSteps.java
@@ -1,12 +1,28 @@
 package cucumberIntegrationTests.stepDefinitions.common;
 
+import io.cucumber.java.en.Given;
 import cucumberIntegrationTests.CreateSessionCucumber;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Parameters;
+
 
 public class BaseSteps {
     CreateSessionCucumber createSessionCucumber;
     public WebDriver driver;
     public String platform;
-    
+
+    @Parameters({"os"})
+    @Given("^User has slideshare \"([^\"]*)\" app$")
+    public void userHasSlideshareApp(String invokeDriver) throws Exception {
+
+        //Use this when running via testng
+        // platform= Reporter.getCurrentTestResult().getTestContext().getCurrentXMLTest().getParameter("os");
+        
+        //Use this when running via junit
+        platform = System.getProperty("platform");
+        createSessionCucumber = new CreateSessionCucumber();
+        createSessionCucumber.createDriver(platform, BaseSteps.class.getDeclaredMethod("userHasSlideshareApp",String.class));
+        driver = createSessionCucumber.getWebDriver();
+    }
 
 }
